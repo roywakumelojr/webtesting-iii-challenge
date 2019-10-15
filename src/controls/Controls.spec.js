100039d404b2af57358a9532e27cf5547c425faa
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-14 20:36:13 -0400

    MVP Has Been Reached

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Controls from './Controls'
+
+it('Checks if the gate control buttons are available for the user', () => {
+    render (<Controls />)
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard'
+
+it('should match snapshot', () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
